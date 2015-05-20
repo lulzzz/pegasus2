29011b1524c3d83171aef0027775655d0523a340
Randy Chapman <randyc@microsoft.com>
2015-05-20 12:23:52 -0700

    Support for Xamarin Studio and switch to XAML

--- Pegasus.Phone/Pegasus.Phone.XF/Pegasus.Phone.XF/App.cs
+++ Pegasus.Phone/Pegasus.Phone.XF/Pegasus.Phone.XF/App.cs
@@ -1,9 +1,4 @@
-﻿using Newtonsoft.Json;
-using Piraeus.Web.WebSockets;
-using Pegasus2.Data;
-using Piraeus.ServiceModel.Protocols.Coap;
-//using Piraeus.Web.WebSockets;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,74 +9,10 @@ namespace Pegasus.Phone.XF
 {
 	public class App : Application
 	{
-        private static string host = "ws://habtest.azurewebsites.net/api/connect";
-        private static string subprotocol = "coap.v1";
-        Label myLabel;
-
-        private void ConnectWebSocket(object sender, EventArgs e)
-        {
-            //WebSocketClient client = new WebSocketClient();
-            var client = DependencyService.Get<IWebSocketClient>();
-            client.OnError += client_OnError;
-            client.OnOpen += client_OnOpen;
-            client.OnClose += client_OnClose;
-            client.OnMessage += client_OnMessage;
-            Task task = Task.Factory.StartNew(async () =>
-                {
-                    await client.ConnectAsync(host, subprotocol, null);
-                });
-
-            Task.WhenAll(task);
-        }
-
-        private static int messageCount;
-
-        private void client_OnMessage(object sender, byte[] message)
-        {
-            CoapMessage coapMessage = CoapMessage.DecodeMessage(message);
-            string jsonString = Encoding.UTF8.GetString(coapMessage.Payload, 0, coapMessage.Payload.Length);
-            CraftTelemetry telemetry = JsonConvert.DeserializeObject<CraftTelemetry>(jsonString);
-            Device.BeginInvokeOnMainThread(() => myLabel.Text = String.Format("{0} ({1} messages)", telemetry.AtmosphericPressure, ++messageCount));
-        }
-
-        private void client_OnClose(object sender, string message)
-        {
-            throw new NotImplementedException();
-        }
-
-        private void client_OnOpen(object sender, string message)
-        {
-            Device.BeginInvokeOnMainThread(() => myLabel.Text = "Web Socket is open");
-        
-        }
-
-        private void client_OnError(object sender, Exception ex)
-        {
-            throw new NotImplementedException();
-        }
- 
 		public App ()
 		{
-            myLabel = new Label
-            {
-                XAlign = TextAlignment.Center,
-                Text = "Welcome to Xamarin Forms!"
-            };
-
-            Button button = new Button { Text = "Go" };
-            button.Clicked += ConnectWebSocket;
- 
-			// The root page of your application
-			MainPage = new ContentPage {
-				Content = new StackLayout {
-					VerticalOptions = LayoutOptions.Center,
-					Children = {
-                        myLabel,
-                        button
-					}
-				}
-			};
-		}
+            MainPage = new Pages.MainPage();
+        }
 
 		protected override void OnStart ()
 		{
--- Pegasus.Phone/Pegasus.Phone.XF/Pegasus.Phone.XF/Pages/MainPage.xaml.cs
+++ Pegasus.Phone/Pegasus.Phone.XF/Pegasus.Phone.XF/Pages/MainPage.xaml.cs
@@ -6,13 +6,71 @@ using System.Threading.Tasks;
 
 using Xamarin.Forms;
 
+using Newtonsoft.Json;
+using Piraeus.Web.WebSockets;
+using Pegasus2.Data;
+using Piraeus.ServiceModel.Protocols.Coap;
+//using Piraeus.Web.WebSockets;
+
 namespace Pegasus.Phone.XF.Pages
 {
-	public partial class MainPage : ContentPage
-	{
-		public MainPage ()
-		{
-			InitializeComponent ();
-		}
-	}
+    public partial class MainPage : ContentPage
+    {
+        private static string host = "ws://habtest.azurewebsites.net/api/connect";
+        private static string subprotocol = "coap.v1";
+
+        public string MainText
+        {
+            get; set;
+        }
+
+        private void ConnectWebSocket(object sender, EventArgs e)
+        {
+            //WebSocketClient client = new WebSocketClient();
+            var client = DependencyService.Get<IWebSocketClient>();
+            client.OnError += client_OnError;
+            client.OnOpen += client_OnOpen;
+            client.OnClose += client_OnClose;
+            client.OnMessage += client_OnMessage;
+            Task task = Task.Factory.StartNew(async () =>
+                {
+                    await client.ConnectAsync(host, subprotocol, null);
+                });
+
+            Task.WhenAll(task);
+        }
+
+        private static int messageCount;
+
+        private void client_OnMessage(object sender, byte[] message)
+        {
+            CoapMessage coapMessage = CoapMessage.DecodeMessage(message);
+            string jsonString = Encoding.UTF8.GetString(coapMessage.Payload, 0, coapMessage.Payload.Length);
+            CraftTelemetry telemetry = JsonConvert.DeserializeObject<CraftTelemetry>(jsonString);
+            Device.BeginInvokeOnMainThread(() => myLabel.Text = String.Format("{0} ({1} messages)", telemetry.AtmosphericPressure, ++messageCount));
+        }
+
+        private void client_OnClose(object sender, string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void client_OnOpen(object sender, string message)
+        {
+            Device.BeginInvokeOnMainThread(() => myLabel.Text = "Web Socket is open");
+        
+        }
+
+        private void client_OnError(object sender, Exception ex)
+        {
+            throw new NotImplementedException();
+        }
+ 
+        public MainPage()
+        {
+            InitializeComponent();
+            this.MainText = "Hello, world";
+            this.BindingContext = this;
+        }
+    }
 }
