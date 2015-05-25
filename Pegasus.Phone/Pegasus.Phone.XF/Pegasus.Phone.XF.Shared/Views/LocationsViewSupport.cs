d309112c0319d35051b56ff74f560bd992e9ea0e
Randy Chapman <randyc@outlook.com>
2015-05-24 20:38:24 -0700

    Show a second pin for ground location

--- Pegasus.Phone/Pegasus.Phone.XF/Pegasus.Phone.XF.Shared/Views/LocationsViewSupport.cs
+++ Pegasus.Phone/Pegasus.Phone.XF/Pegasus.Phone.XF.Shared/Views/LocationsViewSupport.cs
@@ -1,6 +1,7 @@
 ﻿using Pegasus.Phone.XF;
 using Pegasus.Phone.XF.ViewModels.Views;
 using System;
+using System.Collections.Generic;
 using Xamarin.Forms;
 using Xamarin.Forms.Maps;
 
@@ -11,17 +12,16 @@ namespace Pegasus.Phone.XF
     {
         LocationsViewModel viewModel;
         ContentView sourceView;
-        //Map map;
-        Xamarin.Forms.Maps.Map map = new Xamarin.Forms.Maps.Map();
+        Map map;
 
         public void BindToView(ContentView view)
         {
+            map = new Xamarin.Forms.Maps.Map();
+
             sourceView = view;
             // using a bigger frame such as 800x800 for windows store app
             sourceView.WidthRequest = 300;
             sourceView.HeightRequest = 300;
-            //map = new Xamarin.Forms.Maps.Map();
-            //map = new PegasusMap();
             sourceView.Content = map;
 
             sourceView.BindingContextChanged += View_BindingContextChanged;
@@ -34,24 +34,46 @@ namespace Pegasus.Phone.XF
             {
                 viewModel = (LocationsViewModel)sourceView.BindingContext; // Hard-cast to cause an error
                 viewModel.CraftTelemetry.PropertyChanged += CraftTelemetryChanged;
+                viewModel.GroundTelemetry.PropertyChanged += CraftTelemetryChanged;
             }
         }
 
         private void CraftTelemetryChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
         {
-            Xamarin.Forms.Maps.Position craftPosition = new Xamarin.Forms.Maps.Position(
+            if (viewModel.CraftTelemetry.Data == null)
+            {
+                return;
+            }
+
+            while (map.Pins.Count > 0)
+            {
+                map.Pins.RemoveAt(map.Pins.Count - 1);
+            }
+
+            Position craftPosition = new Position(
                     viewModel.CraftTelemetry.Data.GpsLatitude,
                     viewModel.CraftTelemetry.Data.GpsLongitude);
-            Pin pin = new Pin {
-                Type = PinType.Place,
-                Position = craftPosition,
-                Label = "Current Location"
-            };
-            if (map.Pins.Count > 0)
+
+            map.Pins.Add(new Pin()
+                {
+                    Type = PinType.Place,
+                    Position = craftPosition,
+                    Label = "Current Location"
+                });
+
+            if (viewModel.GroundTelemetry.Data != null)
             {
-                map.Pins.RemoveAt(0);
+                Position groundPosition = new Position(
+                        viewModel.GroundTelemetry.Data.GpsLatitude,
+                        viewModel.GroundTelemetry.Data.GpsLongitude);
+
+                map.Pins.Add(new Pin()
+                    {
+                        Type = PinType.Place,
+                        Position = groundPosition,
+                        Label = "Ground Location"
+                    });
             }
-            map.Pins.Add(pin);
 
             MapSpan span = MapSpan.FromCenterAndRadius(craftPosition,
                 Distance.FromMiles(5));
