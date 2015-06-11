007a138c5154cebf6b6ea0e1d6e6531f351eb55b
smartmuki <smartmuki@gmail.com>
2015-06-11 23:25:06 +0530

    1. integrated socket data with view 2. updated css of most components 3. Removed unnecessary widgets

--- PegasusMissionWeb/PegasusMissionWeb/Scripts/base.js
+++ PegasusMissionWeb/PegasusMissionWeb/Scripts/base.js
@@ -1,16 +1,36 @@
 $(function () {
-	
-	
-	$('.subnavbar').find ('li').each (function (i) {
-	
-		var mod = i % 3;
-		
-		if (mod === 2) {
-			$(this).addClass ('subnavbar-open-right');
-		}
-		
-	});
-	
-	
-	
+    $('.subnavbar').find('li').each(function (i) {
+        var mod = i % 3;
+        if (mod === 2) {
+            $(this).addClass('subnavbar-open-right');
+        }
+    });
 });
+
+var mapInitialized = false;
+var mapOptions = {
+    center: new google.maps.LatLng(0, 0),
+    zoom: 10,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+};
+var map = new google.maps.Map(document.getElementById("map_canvas"),
+  mapOptions);
+
+function initialize(ltd, lng) {
+    mapInitialized = true;
+    var latLng = new google.maps.LatLng(ltd, lng);
+    map.panTo(latLng);
+    //// create a marker
+    //var latlng = new google.maps.LatLng();
+    //var marker = new google.maps.Marker({
+    //  position: latlng,
+    //  map: map,
+    //  title: 'My Place'
+    //});
+
+}
+
+function htmlEncode(value) {
+    var encodedValue = $('<div />').text(value).html();
+    return encodedValue;
+}
---
+++ PegasusMissionWeb/PegasusMissionWeb/Scripts/telemetry.js
@@ -0,0 +1,21 @@
+﻿var app = angular.module('pegasus', []);
+
+app.controller('telemetry', ['$scope', function ($scope) {
+        $(document).ready(function () {
+
+        var chat = $.connection.pegasusHub;
+        chat.client.addNewMessageToPage = function (jsonString) {
+            var obj = JSON.parse(jsonString);
+            if (typeof(obj.gpsAltitude) != "undefined") {
+                $.extend($scope, obj);
+                $scope.$apply();
+            }
+
+            initialize(55.770 + Math.random(), 37.424 + Math.random());
+        };
+
+        $.connection.hub.start().done(function () {
+            chat.server.send(1);
+        });
+    });
+}]);
