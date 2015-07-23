0b0cab64cacee9fd0b2ae6320a31ef1ef3e5ac3e
wmattlong <malong@microsoft.com>
2015-07-23 07:33:42 -0500

    Add SMS Notifications page and update Index view

--- PegasusMissionWeb/PegasusMissionWeb/Controllers/HomeController.cs
+++ PegasusMissionWeb/PegasusMissionWeb/Controllers/HomeController.cs
@@ -2,6 +2,8 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
 using System.Web;
 using System.Web.Mvc;
 
@@ -9,6 +11,7 @@ namespace PegasusMissionWeb.Controllers
 {
     public class HomeController : Controller
     {
+        private static string error;
         public ActionResult Index()
         {
             return View();
@@ -16,10 +19,24 @@ namespace PegasusMissionWeb.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "About The Pegasus Mission";
 
             return View();
         }
+
+        public ActionResult Notifications()
+        {
+            ViewBag.Message = "Pegasus II Text Message Notifications";
+            if (!string.IsNullOrEmpty(error))
+            {
+                ViewBag.PhoneError = error;
+                error = null;
+            }
+
+            return View();
+        }
+
+        
 
         public ActionResult ActivateSMS()
         {
@@ -29,6 +46,44 @@ namespace PegasusMissionWeb.Controllers
             //"{\"Number\":\"5302137893\"}"
 
             //string jsonString = String.Format("{\"Number\":\"{0}\"}", number);
+            SMSManager.AddPhone(jsonString);
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public ActionResult AddPhone(string countryCode, string number)
+        {
+            error = null;
+            //^(\(?\+?[0-9]*\)?)?[0-9_\- \(\)]*$
+            //"^(\+[1-9][0-9]*(\([0-9]*\)|-[0-9]*-))?[0]?[1-9][0-9\- ]*$"
+            Regex exp = new Regex(@"^(\(?\+?[0-9]*\)?)?[0-9_\- \(\)]*$");
+
+            StringBuilder builder = new StringBuilder();
+            
+            if(!string.IsNullOrEmpty(countryCode))
+            {
+                if(countryCode.Trim().ToCharArray()[0] != '+')
+                {
+                    builder.Append("+");
+                }
+
+                builder.Append(countryCode.Trim());
+            }
+
+            if(!string.IsNullOrEmpty(number))
+            {
+                builder.Append(number.Trim());
+            }
+
+            string phone = builder.ToString();
+
+            if(!exp.IsMatch(phone))
+            {
+               error = "Phone number is not valid.";
+                return RedirectToAction("Notifications");                
+            }
+
+            string jsonString = "{\"Number\":\"" + phone + "\"}";
             SMSManager.AddPhone(jsonString);
             return RedirectToAction("Index");
         }
