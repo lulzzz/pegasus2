f2870bafa89cc48fe2a683b444141bac3d4a82b2
Randy Chapman <randyc@outlook.com>
2015-07-28 21:15:56 -0700

    Control visibility of up/down arrows on altitude by the vertical speed

---
+++ Pegasus.Phone/Pegasus.Phone.XF/Pegasus.Phone.XF/Converters/VerticalSpeedToVisibilityConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Pegasus.Phone.XF.Converters
+{
+    public class VerticalSpeedToVisibilityConverter : IValueConverter
+    {
+        public bool PositiveIsInvisible { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double? v = value as double?;
+            if (!v.HasValue)
+            {
+                return null;
+            }
+
+            double speed = v.Value;
+
+            return (speed >= 0) ^ PositiveIsInvisible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
