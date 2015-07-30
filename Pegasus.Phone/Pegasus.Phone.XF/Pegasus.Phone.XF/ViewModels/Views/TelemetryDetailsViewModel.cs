8c34897e1c2f93cf52d9e99a8f9f25faace9c8e6
Randy Chapman <randyc@microsoft.com>
2015-07-30 16:09:39 -0700

    Improve the look of the detail view

--- Pegasus.Phone/Pegasus.Phone.XF/Pegasus.Phone.XF/ViewModels/Views/TelemetryDetailsViewModel.cs
+++ Pegasus.Phone/Pegasus.Phone.XF/Pegasus.Phone.XF/ViewModels/Views/TelemetryDetailsViewModel.cs
@@ -10,64 +10,15 @@ namespace Pegasus.Phone.XF.ViewModels.Views
 {
     public class TelemetryDetailsViewModel : BaseViewModel
     {
-        public class TelemetryItem : BaseViewModel
-        {
-            string name;
-            public string Name
-            {
-                get { return name; }
-                set { SetProperty(ref name, value); }
-            }
-
-            string val;
-            public string Value
-            {
-                get { return val; }
-                set { SetProperty(ref val, value); }
-            }
-
-            internal Func<TelemetryDetailsViewModel, string> Formatter;
-        }
-
         public CraftTelemetryViewModel CraftTelemetry
         {
             get;
             private set;
         }
 
-        ObservableCollection<TelemetryItem> telemetryItems;
-        public ObservableCollection<TelemetryItem> TelemetryItems
-        {
-            get { return telemetryItems; }
-        }
-
-        private void TelemetryChanged(object sender = null, System.ComponentModel.PropertyChangedEventArgs e = null)
-        {
-            foreach (var item in TelemetryItems)
-            {
-                item.Value = (this.CraftTelemetry.Data == null) ? "-" : item.Formatter(this);
-            }
-        }
-
         public TelemetryDetailsViewModel()
         {
             CraftTelemetry = App.Instance.CurrentCraftTelemetry;
-
-            telemetryItems = new ObservableCollection<TelemetryItem>();
-            telemetryItems.Add(new TelemetryItem
-            {
-                Name = "GPS Alitutude",
-                Formatter = vm => String.Format("{0} ft", vm.CraftTelemetry.Data.GpsAltitude)
-            });
-            telemetryItems.Add(new TelemetryItem
-            {
-                Name = "GPS Longitude",
-                Formatter = vm => String.Format("{0}", vm.CraftTelemetry.Data.GpsLongitude)
-            });
-
-            // This will cause a memory leak, but since we're only creating one of these...
-            CraftTelemetry.PropertyChanged += TelemetryChanged;
-            TelemetryChanged();
-        }
+       }
     }
 }
