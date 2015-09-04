e2dbb7873bc867b7bc1920a27fc5829bcc9ae259
Randy Chapman <randyc@outlook.com>
2015-09-03 22:42:24 -0700

    Fix app name on android to be "Pegasus Mission" instead of "Pegasus Phone"

--- Pegasus.Phone/Pegasus.Phone.XF/Pegasus.Phone.XF.Droid/MainActivity.cs
+++ Pegasus.Phone/Pegasus.Phone.XF/Pegasus.Phone.XF.Droid/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace Pegasus.Phone.XF.Droid
 {
-    [Activity(Label = "Pegasus Phone", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Pegasus Mission", Icon = "@drawable/icon", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsApplicationActivity
     {
         protected override void OnCreate(Bundle bundle)
